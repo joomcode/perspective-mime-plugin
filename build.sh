80f46364ad8ea57a69ed46f52bfdf6d16c040c12
Denis Goihburg <gburg@joom.com>
2022-10-10 23:22:20 +0400

    Add build script

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ VERSION=0.1.0
 yarn install && \
   yarn build && \
   jupyter labextension build  && \
-  zip -r perspective-mime.zip labextension && \
-  aws s3 cp perspective-mime.zip s3://joom-analytics-deploy/platform/perspective-mime/perspective-mime-$VERSION.zip
+  tar -czvf perspective-mime.tar.gz perspective-mime && \
+  aws s3 cp perspective-mime.tar.gz s3://joom-analytics-deploy/platform/perspective-mime/perspective-mime-$VERSION.tar.gz
